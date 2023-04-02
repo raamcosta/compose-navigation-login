329c5ea15882827c4a3ce9769b76c2da8b35df06
stylianosgakis <stylianos.gakis98@gmail.com>
2023-04-02 23:01:04 +0300

    Use collectAsStateWithLifecycle
    To avoid still observing the StateFlow in the background and potentially trying to navigate around while the app isn't foregrounded.

--- app/src/main/java/com/example/navigationtests/MainActivity.kt
+++ app/src/main/java/com/example/navigationtests/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -58,9 +61,9 @@ object AuthenticatedScreenWrapper: DestinationWrapper {
     @Composable
     override fun <T> DestinationScope<T>.Wrap(screenContent: @Composable () -> Unit) {
         val diContainer = LocalDiContainer.current
-        val userRepo = remember { diContainer.userRepository }
-
-        when (val userState = userRepo.loggedInUser.collectAsState().value) {
+        val userRepo = remember(diContainer) { diContainer.userRepository }
+        val userState by userRepo.loggedInUser.collectAsStateWithLifecycle()
+        when (userState) {
             is UserRepository.UserState.Loading -> Box(Modifier.fillMaxSize()) {
                 Text(
                     modifier = Modifier.align(Alignment.Center),
